f5cb50237b4be91f04d91efa0917ca43c527dddb
H4143 <H4143@ELEK-436-WS-08>
2017-09-27 10:59:47 +0300

    Aloitettu tehtävä 4

---
+++ lab05/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.Nimi = "Kirsi";
+            employee.Salary = 50;
+            Console.WriteLine(employee.Tiedot());
+        }
+    }
+}
