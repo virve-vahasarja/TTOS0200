b14dd7468ee8e5cfe834ab94337df982e52f1bfc
H4143 <H4143@ELEK-436-WS-08>
2017-11-15 10:49:58 +0200

    Autotallitehtävä vk 46.  valmis ja toimii

---
+++ autotalli/autotalli/BLAutotalli.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+   public static class Autotalli
+    {
+        public static List<Auto> HaeAutot()
+        {
+            List<Auto> autot = new List<Auto>();
+            //gui testaamista varten keksitään muutama auto
+            Auto a = new Auto();
+            a.Merkki = "Volvo";
+            a.Malli = "V70";
+            a.VM = 1970;
+            a.KM = 400000;
+            a.Hinta = 9000F;
+            a.URL = "VolvoV70.png";
+            autot.Add(a);
+
+            //toinenkin auto
+            Auto b = new Auto()
+            {
+                Merkki = "Audi", Malli = "A4", VM = 2010, KM = 100000, Hinta = 50, URL = "AudiA4.png"
+            };
+            autot.Add(b);
+             
+         
+
+            //toinenkin auto
+            Auto c = new Auto()
+            {
+                Merkki = "Audi",
+                Malli = "A3",
+                VM = 2000,
+                KM = 100000,
+                Hinta = 50
+            };
+            autot.Add(c);
+
+            //neljäs auto
+            Auto d = new Auto()
+            {
+                Merkki = "Lada",
+                Malli = "Samara",
+                VM = 1989,
+                KM = 100000,
+                Hinta = 50,
+                URL="samara.jpg"
+            };
+            autot.Add(d);
+
+            //palautus
+            return autot;
+        }
+    }
+    public class Auto
+    {
+        public string Merkki { get; set;  }
+        public string Malli { get; set; }
+        public int VM { get; set; }
+        public int KM { get; set; }
+        public float Hinta { get; set; }
+        public string URL { get; set; }
+    }
+}
---
+++ autotalli/autotalli/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using JAMK.IT;
+
+namespace autotalli
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Auto> autot;//muuttuja on käytettävissä kaikissa tämän luokan metodeissa ja tapahtumankäsittelijöissä
+
+        private const string polku = @"d:\\H4143\TTOS0200\kuvia\\";
+            public MainWindow()
+        {
+            //tänne koodi joka suoritetaan ikkunan avauksen luonnissa
+            InitializeComponent();
+           
+            //pyydetään BL-kerrokselta autot ja näytetään ne käyttäjälle kutsutaan staattista metodia
+            autot = JAMK.IT.Autotalli.HaeAutot();
+
+            NaytaKuva("autotalli.png");
+        }
+
+        private void btnHaeAutot_Click(object sender, RoutedEventArgs e)
+        {
+            dgAutot.ItemsSource = autot;
+
+        }
+        private void NaytaKuva(string url)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(url))
+                {
+                    url = "puuttuu.png";
+                }
+                //lisätään kuvatiedostojen vakiopolku
+                url = polku + url;
+                //kuvan näyttö
+                BitmapImage pic = new BitmapImage();
+                pic.BeginInit();
+                pic.UriSource = new Uri(url);
+                pic.EndInit();
+                imgAuto.Stretch = Stretch.Fill;
+                imgAuto.Source = pic;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dgAutot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //huom. olemme itse populoineet DataGridin Auto-olioilla joten kukin DataGridin jäsen/alkio on itseasiassa Auto-olio
+            Object obj = dgAutot.SelectedItem;
+            if (obj!=null)
+            {
+                Auto valittu = (Auto)obj;
+                NaytaKuva(valittu.URL);
+            }
+        }
+
+        private void btnHaeAudit_Click(object sender, RoutedEventArgs e)
+        {
+            //näkyviin audit
+            var result = autot.Where(m => m.Merkki.Contains("Audi"));
+            dgAutot.ItemsSource = result;
+        }
+    }
+
+}
